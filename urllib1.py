ffb019bcb9c6f78a22b471b083c1e8bbe5595c6f
teguhpambudif <teguhpambudi1251@gmail.com>
2023-01-11 21:05:39 +0700

    3 py program

---
+++ urllib1.py
@@ -0,0 +1,7 @@
+# this code below from coursera-courses
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+for line in fhand:
+    print(line.decode().strip())
+
